440d29a8f8c59ee8204b6e38c821b484d544a1dd
Vlada926 <vvmvladmer1@example.com>
2026-06-14 12:22:31 +0000

    update Lviv sales to 950

--- analysis.py
+++ analysis.py
@@ -1,1 +1,11 @@
+import pandas as pd
+
+data = {
+    "city": ["Kyiv", "Lviv", "Odesa"], "sales":
+  }
+df = pd.DataFrame(data)
+
 print("Продажі по містах:")
+print(df)
+
+print("Середнє значення:", df["sales"].mean())
